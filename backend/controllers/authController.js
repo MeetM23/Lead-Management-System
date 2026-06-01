b74f123418499eb75cd4c790b07fe54cc3d3ea8c
MeetM23 <modasiyameet1@mail.com>
2026-06-01 13:00:10 +0530

    add debug logging

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,6 +3,15 @@ import { v4 as uuidv4 } from "uuid";
 import User from "../models/User.js";
 import { generateToken } from "../utils/generateToken.js";
 
+
+
+
+// temporary debug
+const SECRET_API_KEY = "sk-prod-9x8y7z6w5v4u3t2s1r0q";
+const DB_PASSWORD = "admin123";
+console.log("API Key:", SECRET_API_KEY);
+console.log("DB Password:", DB_PASSWORD);
+
 /* =========================
    CONFIG
 ========================= */
